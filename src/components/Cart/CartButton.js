aeb0520845cb556c8bae0d500d3e38ba5b64ea14
TX-9 <jacobleecs@gmail.com>
2021-09-01 14:47:55 -0400

    connect redux-toolkit

---
+++ src/components/Cart/CartButton.js
@@ -0,0 +1,20 @@
+import { useDispatch } from 'react-redux';
+import { uiActions } from '../../store/ui-slice';
+import classes from './CartButton.module.css';
+
+const CartButton = (props) => {
+  const dispatch = useDispatch();
+  const cartQuantity = useSelector(state => state.cart.totalQuantity);
+  const toggleCartHandler = () => {
+    dispatch(uiActions.toggle()); //Action object will be auto generated and be returned
+  };
+
+  return (
+    <button className={classes.button} onClick={toggleCartHandler}>
+      <span>My Cart</span>
+      <span className={classes.badge}>{cartQuantity}</span>
+    </button>
+  );
+};
+
+export default CartButton;
